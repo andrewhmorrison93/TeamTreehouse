4bf8c8b24bf39a37ae9afa32faf2554d7b692e4a
andrewhmorrison93 <andrewhmorrison93@gmail.com>
2015-10-20 16:06:56 -0400

    project complete

--- Node.js Basics Project/app.js
+++ Node.js Basics Project/app.js
@@ -1,2 +1,9 @@
-//Problem: We need a simple way to look at a user's badge count and Javascript points
-//Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+//require the profile.js file
+//.js at end here is optional, but the path ("./") is not optional!
+var profile = require("./profile.js");
+
+//grab users that were entered when the application was run [e.g., node app.js user1 user2] and then assign those to the users variable
+var users = process.argv.slice(2);
+
+//run method to look up username
+users.forEach(profile.get);
